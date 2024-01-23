5e1c711b4bd3724210e14d62eacaada3de5a327f
Ahmedsaid3 <gulsena22@itu.edu.tr>
2024-01-23 19:03:54 +0300

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,23 @@
+import random
+print('Welcome to the password generator :D')
+symbols = ['!','@','#','$','%','^','&','*','(',')','~','/','?','>','<']
+letters = ['a','s','d','f','g','h','j','k','l','m','n','b','v','c','x','z','q','w','e','r','t','y','u','i','o','p','A','S','D','F','G','H','J','K','L','P','O','I','U','Y','T','R','E','W','Q','Z','X','C','V','B','N','M']
+numbers = ['0','1','2','3','4','5','6','7','8','9']
+symbol = int(input('How many symbols would you like in your password: '))
+letter = int(input('How many letters would you like in your password: '))
+number = int(input('How many numbers would you like in your password: '))
+ls = []
+def password_generator(symbols, letters, numbers):
+    for _ in range(symbol):
+        selected_item = random.choice(symbols) 
+        ls.append(selected_item)
+    for _ in range(letter):
+        selected_item = random.choice(letters) 
+        ls.append(selected_item)
+    for _ in range(number):
+        selected_item = random.choice(numbers) 
+        ls.append(selected_item)
+password_generator(symbols, letters, numbers)
+random.shuffle(ls)
+string = ''.join(ls)
+print(string)
---
+++ rock_paper_scissors.py
@@ -0,0 +1,47 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+import random
+ls = [rock,paper,scissors]
+COMPUTERS_CHOICE = random.randint(0,2)
+while True:
+    YOUR_CHOICE = int(input('0 for stone, 1 for paper, 2 for scissors: '))
+    number = ls[YOUR_CHOICE]
+    print(f"Computer's choice :\n {ls[COMPUTERS_CHOICE]}")
+    print(f'Your choice :\n {number}')
+    
+    if YOUR_CHOICE == 2 and COMPUTERS_CHOICE == 0:
+        print('You lost :(')
+    elif YOUR_CHOICE == 0 and COMPUTERS_CHOICE == 2:
+        print('You win :D')   
+    elif YOUR_CHOICE > COMPUTERS_CHOICE:
+        print('You win :D')
+    elif YOUR_CHOICE == COMPUTERS_CHOICE:
+        print('Draw :|')
+    else:
+        print('You lost :(')
